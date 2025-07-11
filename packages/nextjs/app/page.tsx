9f06037c6fcd0cf5c89047e0a3e18fd556ca46cd
diegoprego <144163801+diegoprego@users.noreply.github.com>
2025-07-11 16:22:23 -0300

    Update page.tsx

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { BugAntIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
@@ -15,19 +15,20 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">SimpleSwap Test App</span>
           </h1>
           <div className="flex justify-center items-center space-x-2 flex-col">
             <p className="my-2 font-medium">Connected Address:</p>
             <Address address={connectedAddress} />
           </div>
-
+          {/*
           <p className="text-center text-lg">
             Get started by editing{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
               packages/nextjs/app/page.tsx
             </code>
           </p>
+          
           <p className="text-center text-lg">
             Edit your smart contract{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
@@ -38,6 +39,7 @@ const Home: NextPage = () => {
               packages/hardhat/contracts
             </code>
           </p>
+          */}
         </div>
 
         <div className="grow bg-base-300 w-full mt-16 px-8 py-12">
@@ -45,13 +47,14 @@ const Home: NextPage = () => {
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <BugAntIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Tinker with your smart contract using the{" "}
+                Interact with SimpleSwap & Tokens using the{" "}
                 <Link href="/debug" passHref className="link">
-                  Debug Contracts
+                  Contracts
                 </Link>{" "}
                 tab.
               </p>
             </div>
+            {/*
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
               <p>
@@ -62,6 +65,7 @@ const Home: NextPage = () => {
                 tab.
               </p>
             </div>
+            */}
           </div>
         </div>
       </div>
@@ -70,3 +74,4 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
